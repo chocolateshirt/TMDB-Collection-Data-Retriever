cf30c005497acc3058c36f9a93d8dbb6290c9a99
Iqbal Djoyowiyono <iqbaldjoyowiyono@gmail.com>
2019-03-13 16:11:42 +0700

    Update collection_updater.py

--- collection_updater.py
+++ collection_updater.py
@@ -147,8 +147,9 @@ def main():
 
             if lang == 'en':
                 plex_col_title = plex_col_dict['title'] + ' Collection'
+                plex_col_title2 = plex_col_dict['title']
 
-            if tmdb_col_dict['name'] != plex_col_title:
+            if tmdb_col_dict['name'] != plex_col_title and tmdb_col_dict['name'] != plex_col_title2:
                 print('  Invalid collection, does not match with the TMDB collection: %s' % tmdb_col_dict['name'])
 
                 err_list.append('\r\n> %s' % plex_col_title)
